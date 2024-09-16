0fd72d295ebb380832322d445233697a00ce1282
balajishiva2001 <balajishiva2001@gmail.com>
2024-09-16 22:12:20 +0530

    sample-springsecurity-oauth2login-project

---
+++ src/main/java/com/samplespringsecurityoauth2login/app/SampleSpringsecurityOauth2loginProjectApplication.java
@@ -0,0 +1,13 @@
+package com.samplespringsecurityoauth2login.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringsecurityOauth2loginProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringsecurityOauth2loginProjectApplication.class, args);
+	}
+
+}
